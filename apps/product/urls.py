71731f934f266c9b88109d3c2c99e89172e34a56
Alymbekov <alymbekovdastan1@gmail.com>
2021-01-05 15:21:18 +0600

    add detail view

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -6,4 +6,9 @@ app_name = 'product'
 
 urlpatterns = [
     path('', views.ListProductView.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
+
+#<str:slug>
+#<uuid:key>
+
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from apps.product.models import Product
 
@@ -7,3 +7,13 @@ from apps.product.models import Product
 class ListProductView(ListView):
     model = Product
     template_name = 'product/product_list.html'
+    context_object_name = 'products'
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     super
+    def get_queryset(self):
+        return Product.objects.filter(in_stock=True)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product/product_detail.html'
+    context_object_name = 'product'
